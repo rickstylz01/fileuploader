b2622f39630e21fb65bf294d4c4e5157c46ece20
Rick Maya <rickmaya@gmail.com>
2021-01-30 11:58:59 -0600

    add searc bar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
+import './App.css';
+import SearchBar from './SearchBar/SearchBar';
+import FilterableList from './FilterableList/FilterableList';
 
-function App() {
-  return (
-    <main className='App'>
-      {/* content goes here */}
-    </main>
-  );
+class App extends Component() {
+  render() {
+    return (
+      <div className="App">
+        <SearchBar />
+        <FilterableList files={this.props.files} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,4 +3,11 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const FILES = [
+  {"fileType":"jpg", "size":"4.3MB", "name":"me on skis.jpg", "status":"Synced"},
+  {"fileType":"mov", "size":"1.3GB", "name":"cats falling.mov", "status":"Uploaded"},
+  {"fileType":"txt", "size":"0.9KB", "name":"My December expenses.txt","status":"Uploaded"},
+  {"fileType":"mp3", "size":"3.4MB", "name":"disturbed_sound_of_silence.mp3","status":"New"},
+];
+
+ReactDOM.render(<App files={FILES} />, document.getElementById('root'));
